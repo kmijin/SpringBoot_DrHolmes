242c8ef87319ed90fbda6f9b32cfca41382541e9
hyewon <qw4869@naver.com>
2022-06-03 11:36:34 +0900

    로그인 기능 구현

--- app/src/main/java/com/pillgood/drholmes/api/login/Post.java
+++ app/src/main/java/com/pillgood/drholmes/api/login/Post.java
@@ -1,40 +1,66 @@
 package com.pillgood.drholmes.api.login;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Post {
 
-    String userId;
-    String userPassword;
-    String userName;
-    String userBirth;
-    String userGender;
-    Integer emailAgree;
-    String joinDate;
+//    String userId;
+//    String userPassword;
+//    String userName;
+//    String userBirth;
+//    String userGender;
+//    Integer emailAgree;
+//    String joinDate;
+//
+//    public Post(String userId, String userPassword) {
+//        this.userId = userId;
+//        this.userPassword = userPassword;
+//    }
+//
+//    public String getId() {
+//        return userId;
+//    }
+//
+//    public String getPassword() {
+//        return userPassword;
+//    }
+//
+//    public String getName() {
+//        return userName;
+//    }
+//
+//    public String getBirth() {
+//        return userBirth;
+//    }
+//
+//    public String getGender() {
+//        return userGender;
+//    }
+//
+//    public Integer getEmailAgree() {
+//        return emailAgree;
+//    }
+//
+//    public String getJoinDate() {
+//        return joinDate;
+//    }
 
-    public String getId() {
-        return userId;
+    public Post(String id, String pw) {
+        this.userId = id;
+        this.userPassword = pw;
+        this.userName = "id";
+        this.userBirth = "pw";
+        this.userGender = "id";
+        this.emailAgree = "pw";
+        this.joinDate = "id";
     }
 
-    public String getPassword() {
-        return userPassword;
-    }
+    @SerializedName("userId") private String userId;
+    @SerializedName("userPassword") private String userPassword;
+    @SerializedName("userName") private String userName;
+    @SerializedName("userBirth") private String userBirth;
+    @SerializedName("userGender") private String userGender;
+    @SerializedName("emailAgree") private String emailAgree;
+    @SerializedName("joinDate") private String joinDate;
 
-    public String getName() {
-        return userName;
-    }
-
-    public String getBirth() {
-        return userBirth;
-    }
-
-    public String getGender() {
-        return userGender;
-    }
-
-    public Integer getEmailAgree() {
-        return emailAgree;
-    }
-
-    public String getJoinDate() {
-        return joinDate;
-    }
 }
